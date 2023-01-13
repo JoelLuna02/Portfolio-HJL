4b41a7d4fa22ad936bab773a84eb53b7a958c79f
JoelLuna02 <hectorjoelluna1234@gmail.com>
2023-01-13 18:15:35 -0300

    Archivos de Frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from "ngx-spinner";
+import { Router, NavigationStart, NavigationEnd, Event } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Portfolio';
+export class AppComponent implements OnInit {
+    constructor(private spinner: NgxSpinnerService, private router: Router) {}
+
+    ngOnInit() {
+        this.router.events.subscribe((event: Event) => {
+            if (event instanceof NavigationStart) {
+                this.spinner.show();
+            }
+            if (event instanceof NavigationEnd) {
+                this.spinner.hide();
+            }
+        })
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgxSpinnerModule } from "ngx-spinner";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,6 +20,7 @@ import { FooterComponent } from './comps/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgxSpinnerModule,
     BrowserAnimationsModule
   ],
   providers: [],
